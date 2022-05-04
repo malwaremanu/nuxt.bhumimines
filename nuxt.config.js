41ffb16544a0f8518d1b2a4ad626dd6fd41b7dde
Manupal Choudhary <tech.manujpr@gmail.com>
2022-05-04 11:58:35 +0530

    Base Router Revoked

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-      base: '/nuxt.bhumimines/'
-    },
+  // router: {
+  //     base: '/nuxt.bhumimines/'
+  //   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Bhumi Mines SARL',
